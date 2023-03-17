6bc38de68d154662c090b786da46c2390f67c6ac
manmeenkaur <manmeen.kaur.17@gmail.com>
2023-03-16 20:11:13 -0400

    Initial Commit

---
+++ MAPD724-W2023-ICE7-ManmeenKaur/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAPD724-W2023-ICE7-ManmeenKaur
+//
+//  Created by Manmeen Kaur on 2023-03-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
